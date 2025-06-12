816da55ae385db5586990a12f485170524de329d
Дмитрий Звонарев <zvonarev@cz71.ru>
2025-06-12 19:38:52 +0300

    add grab

--- WarGame/Forms/FormMain.cs
+++ WarGame/Forms/FormMain.cs
@@ -1,6 +1,9 @@
 
-using WarGame.Resources;
+using CefSharp.OffScreen;
+using System.Windows.Forms;
 using WarGame.Other;
+using WarGame.Resources;
+using System.Drawing;
 
 namespace WarGame;
 
@@ -28,8 +31,34 @@ public sealed partial class FormMain : Form
         _dx.Dispose();
     }
 
-    private void FormShown(object? sender, EventArgs e)
+    private async void FormShown(object? sender, EventArgs e)
     {
+        var zoom = 19;
+        var tileX0 = 316927;
+        var tileY0 = 164368;
+        var tileSize = 256;
+        var topMenuHeight = 38;
+        var lat = GeoMap.LatForTile(zoom, tileX0, tileY0);
+        var lon = GeoMap.LonForTile(zoom, tileX0, tileY0);
+        //var x = GeoMap.TileXForLon(19, lon);
+        //var y = GeoMap.TileYForLat(19, lat);
+        var completionSource = new TaskCompletionSource<Bitmap>();
+        var wb = new ChromiumWebBrowser(string.Empty) { Size = new Size(tileSize * 51, tileSize * 51) };
+        await wb.LoadUrlAsync($"https://bestmaps.ru/map/yandex/satellite/{zoom}/{lat.ToString().Replace(',', '.')}/{lon.ToString().Replace(',', '.')}");
+        await Task.Delay(80000);
+
+        using var bm = wb.ScreenshotOrNull(PopupBlending.Main);
+        using var mat = OpenCvSharp.Extensions.BitmapConverter.ToMat(bm);
+        for (var x = -25; x < 25; x++)
+        {
+            for (var y = -25; y < 25; y++)
+            {
+                using var mm = mat.SubMat(new OpenCvSharp.Rect(tileSize * x + mat.Width / 2, tileSize * y + mat.Height / 2 + topMenuHeight / 2, tileSize, tileSize));
+                mm.SaveImage($"{zoom}_{tileX0 + x}_{tileY0 + y}.png");
+            }
+        }
+        mat.SubMat(new OpenCvSharp.Rect(tileSize * -25 + mat.Width / 2, tileSize * -25 + mat.Height / 2 + topMenuHeight / 2, tileSize * 50, tileSize * 50)).SaveImage($"{zoom}_tiles_all.png");
+        //bm.Save("test.png");
         _ = _dx.StartAsync(default);
     }
 
